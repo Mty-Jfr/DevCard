fe403881b80f660fd80878424039bf4fdaddf0ce
mty <mty.jfr69@gmail.com>
2025-08-04 10:14:06 -0800

    projects section from index page moved to partial view Projects

---
+++ DevCard_MVC/Models/Article.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+namespace DevCard_MVC.Models
+{
+    public class Article
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ DevCard_MVC/ViewComponents/ProjectViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class ProjectViewComponent : ViewComponent
+    {
+        //public IViewComponentResult Invoke()
+        //{
+        //    return ViewComponent("_Projects");
+        //}
+
+       
+    }
+}
